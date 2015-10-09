5ad7dc9339bc41760a72a791c3cf00eedc6fe5dc
heysep <sdvakili@gmail.com>
2015-10-09 14:11:49 -0400

    Modified the create_lists migration and create its class in lists.rb.

--- db/migrate/003_create_lists.rb
+++ db/migrate/003_create_lists.rb
@@ -1,8 +1,10 @@
 class CreateLists < ActiveRecord::Migration
 	def up
 		create_table :lists do |t|
+			t.belongs_to :user
 			t.integer :user_id, null: false
 			t.string :name, null: false
+			t.timestamps null: false
 		end
 	end
 
---
+++ lib/too_done/list.rb
@@ -0,0 +1,5 @@
+module TooDone
+	class List < ActiveRecord::Base 
+		has_many :items
+	end
+end
